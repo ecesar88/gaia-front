31141f0dfc2eec22dfab7376887d31a0986295b7
Erick Cesar <e.cesar88@gmail.com>
2024-02-29 14:07:31 -0300

    chore: update endpoint paramter

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,4 @@
 export default {
   SELECT: (query: string) =>
-    `/gaia/select?indent=true&q.op=OR&q=dominio%3A*${query}*%20OR%20descricao%3A*${query}*%20OR%20metadados%3A*${query}*`,
+    `/gaia/select?indent=true&rows=1000&q.op=OR&q=dominio%3A*${query}*%20OR%20descricao%3A*${query}*%20OR%20metadados%3A*${query}*`,
 } as const;
